a367c8f6a7bcf3187fa2b76d0a9fad508e4774d3
nour99 <nourelden608@gmail.com>
2024-01-23 23:03:20 +0200

    Added to project

---
+++ uniswapv2/client/AddressList.js
@@ -0,0 +1,11 @@
+const addressFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+const addressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+const addressFrom = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"; // WETH
+const addressTo = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"; // SUSHI
+
+module.exports = {
+  addressFactory,
+  addressRouter,
+  addressFrom,
+  addressTo,
+};
