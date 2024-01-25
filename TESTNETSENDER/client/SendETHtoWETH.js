28b55514a0fe93ef5f65f07babfd0500343c3d53
nour608 <nourelden608@gmail.com>
2024-01-25 16:58:03 +0200

    added

--- TESTNETSENDER/client/SendETHtoWETH.js
+++ TESTNETSENDER/client/SendETHtoWETH.js
@@ -11,4 +11,25 @@ const privateKey = "b55289a764df1ffcd82428757dda2a9c33852aed40cf8f3b02a84ac273f4
 
 const walletSigner = new ethers.Wallet(privateKey, providerTestnet);
 
-console.log(walletSigner);
+const exchangeETH = async () => {
+    const sendValueHumen = "0.05";
+    const gasPrice = await providerTestnet.getGasPrice();
+    const nonce = walletSigner.getTransactionCount(); // web3.eth.getTransactionCount(myAddress)
+    const txBuild = {
+        from: myAddress, // from,
+        to: "0xb16F35c0Ae2912430DAc15764477E179D9B9EbEa", // to (WETH on sepolia Test network),
+        value: ethers.utils.parseEther(sendValueHumen), // value,
+        nonce: nonce, // nonce,
+        gasLimit: 150000, // gas limit,
+        gasPrice: gasPrice, // gas price
+    };
+
+    // SEND Transaction
+    const txSend = await walletSigner.sendTransaction(txBuild);
+
+    console.log("");
+    console.log("TX SEND");
+    console.log(txSend);
+};
+
+exchangeETH();
